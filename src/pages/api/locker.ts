29a8771e4aceb1ab8d9ad14cf86d07099d1a4f43
Souhail Benlhachemi <souhailbenlhachemi1999@gmail.com>
2023-06-15 17:39:57 +0100

    locker

--- src/pages/api/locker.ts
+++ src/pages/api/locker.ts
@@ -8,9 +8,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!locale) return res.status(400).send('Bad Request');
 
   // SEND DATA
-  if (locale === 'pr') return res.status(200).json({ locker: 'https://appinstallcheck.com/cl/i/5k8l2v' });
-  if (locale === 'fr') return res.status(200).json({ locker: 'https://locked2.com/o/hxomt4i' });
+  if (locale === 'fr') return res.status(200).json({ locker: 'https://appinstallcheck.com/cl/i/378jp7' })
+  if (locale === 'ar') return res.status(200).json({ locker: 'https://appinstallcheck.com/cl/i/x6jlrg' })
+  if (locale === 'tr') return res.status(200).json({ locker: 'https://appinstallcheck.com/cl/i/378jn7' })
+  if (locale === 'es') return res.status(200).json({ locker: 'https://appinstallcheck.com/cl/i/r7k434' })
+  if (locale === 'id') return res.status(200).json({ locker: 'https://appinstallcheck.com/cl/i/kl5ngq' })
+  if (locale === 'pr') return res.status(200).json({ locker: 'https://appinstallcheck.com/cl/i/nd3eg4' })
   return res
     .status(200)
-    .json({ locker: 'https://www.wauwfy.com/go.php?oid=16746&t=d&tid=13471&sid=' });
+    .json({ locker: 'https://appinstallcheck.com/cl/i/dv53g7' });
 }
